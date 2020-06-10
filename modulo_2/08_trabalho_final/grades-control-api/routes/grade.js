47cb41340852969c4a9b0bb2d759a5f76ad10157
Anderson Ricardo Alves <andersonricardo.alves@gmail.com>
2020-06-10 20:06:44 -0300

    Criada rota para Atualizar Grade -> PUT /

--- modulo_2/08_trabalho_final/grades-control-api/routes/grade.js
+++ modulo_2/08_trabalho_final/grades-control-api/routes/grade.js
@@ -32,4 +32,38 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Atualizar Grade
+router.put('/', async (req, res) => {
+  try {
+    const dtGrades = await getGrades();
+    const newGrade = req.body;
+
+    let oldIndex = dtGrades.grades.findIndex(
+      (grade) => grade.id === newGrade.id
+    );
+
+    if (oldIndex < 0) {
+      throw new Error(`Id '${newGrade.id}' não cadastrado`);
+    }
+
+    dtGrades.grades[oldIndex] = newGrade;
+    await writeFile(global.dataFileName, JSON.stringify(dtGrades));
+
+    res.status(204).send();
+  } catch (err) {
+    const msgError = { error: err.message };
+    res.status(400).send(msgError);
+    console.log(msgError);
+  }
+});
+
 export default router;
+
+/*
+ try {
+
+  } catch (err) {
+    const msgError = { error: err.message };
+    res.status(400).send(msgError);
+    console.log(msgError);
+  }*/
